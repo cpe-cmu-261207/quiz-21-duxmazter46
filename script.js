243ec3983d8066318935c5a2b230f46007d8b1ad
unknown <becktheerit@gmail.com>
2022-08-04 14:11:09 +0700

    TT

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  document.getElementById("div-user-card").style.display = "";
+
+  document.getElementById("img-profile").src =
+    resp.data.results[0].picture.large;
+  document.getElementById("p-name").innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  document.getElementById("p-email").innerText = resp.data.results[0].email;
+  document.getElementById("p-address").innerText = resp.results[0].city;
 }
+
+callApi();
+
+document.getElementById("btn-random").onclick = () => {};
